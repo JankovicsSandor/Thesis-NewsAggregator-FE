91f34f2fdffd7ae02b685b5de90f6b78c5790a2c
JankovicsSandor <sanyika717022@gmail.com>
2020-05-11 14:00:45 +0200

    Modify news item template

--- projects/viewer-components/src/lib/news-item/news-item.module.ts
+++ projects/viewer-components/src/lib/news-item/news-item.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NewsItemComponent } from './news-item.component';
 import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [NewsItemComponent],
-  imports: [CommonModule, MatCardModule],
+  imports: [CommonModule, MatCardModule, MatButtonModule],
   exports: [NewsItemComponent]
 })
 export class NewsItemModule {}
